aa0b37587c121edb2e3e1730b3be68000e2e3a64
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-08-05 06:27:06 +0530

    Commit Morning

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient{
         super({
             datasources:{
                 db:{
-                    url:"postgresql://user:password@localhost:5342/mydatabase?schema=public"
+                    url:"postgresql://healthnexus_user:2soIvojaMf9pTQR7nJdPOv6OMNCwlq0A@dpg-cqnvl1rv2p9s73aidj10-a.oregon-postgres.render.com/healthnexus"
                 }
             }
         })
